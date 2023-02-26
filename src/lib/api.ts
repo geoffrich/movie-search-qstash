aaf422587226302d82db653269a65adc8743296b
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2023-02-25 19:55:43 -0800

    Move API code to separate file

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,6 +2,29 @@ import { error } from '@sveltejs/kit';
 import { TMDB_API_KEY } from '$env/static/private';
 
 import { cacheMovieResponse } from '$lib/redis';
+import type { SearchResponse } from '$lib/types/tmdb';
+import { cacheMovieIds } from '$lib/redis';
+
+const VOTE_THRESHOLD = 20;
+
+export async function searchMovies(searchQuery: string, page: number) {
+	const response = await fetch(
+		`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&page=${page}&include_adult=false&query=${searchQuery}`
+	);
+	const parsed: SearchResponse = await response.json();
+
+	// filter out obscure movies
+	const filteredMovies = parsed.results.filter((movie) => movie.vote_count >= VOTE_THRESHOLD);
+	const removedMovies = parsed.results.filter((movie) => movie.vote_count < 20);
+	console.log(
+		'Filtered out:',
+		removedMovies.map((m) => m.title)
+	);
+
+	await cacheMovieIds(filteredMovies.map((m) => m.id));
+
+	return parsed;
+}
 
 export async function getMovieDetailsFromApi(id: number) {
 	const [movieResponse, creditsResponse] = await Promise.all([getMovieDetails(id), getCredits(id)]);
